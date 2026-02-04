3620219a0d9edc8676bf3559b7592150d1c2f298
phutinat45 <phutinat.su45@gmail.com>
2026-02-04 14:47:18 +0700

    Delete src directory

--- src/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-// เปลี่ยนจากใส่รหัสตรงๆ มาเป็นแบบนี้ครับ
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
